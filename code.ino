45cfc517701ab06e5967bed21da5e98145e85b12
dshedd25 <99275733+dshedd25@users.noreply.github.com>
2022-04-26 20:48:41 -0600

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,20 @@
+/*
+
+*/
+
+void setup() {
+   
+    
+}
+
+void loop() {
+     pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    delay(5000);
+    digitalWrite(8, LOW);
+     digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      delay(5000);
+    
+}
